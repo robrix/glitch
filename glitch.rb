5d6a58ef2248afb3cf3a255e2fc9f3bca5448057
Rob Rix <rix.rob@gmail.com>
2009-05-13 03:09:44 -0400

    Added constants for where to find the repositories to list.

--- glitch.rb
+++ glitch.rb
@@ -2,11 +2,14 @@
 require "rubygems"
 require "sinatra"
 
+REPOSITORIES_ROOT = "/var/repositories"
+REPOSITORIES = ["*"]
+
 get '/' do
-	@repositories = ["."]
+	@repositories = [REPOSITORIES.collect{ |repo| Dir.glob("#{REPOSITORIES_ROOT}/#{repo}") }.flatten]
 	erb :repositories
 end
 
 get '/repositories/:name' do |name|
-	erb :repository, :locals => {:repository => "/var/repositories/#{name}"}
+	erb :repository, :locals => {:repository => Grit::Repo.new("#{REPOSITORIES_ROOT}/#{name}")}
 end
